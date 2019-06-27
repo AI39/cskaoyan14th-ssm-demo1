475e318feea85837934a04263a55ca558971febf
baxigui <yyc05060808@gmail.com>
2019-06-27 15:17:29 +0800

    整合到一个controller

--- module1/src/main/java/com/cskaoyan14th/controller/QualifyController.java
+++ module1/src/main/java/com/cskaoyan14th/controller/QualifyController.java
@@ -1,10 +1,9 @@
 package com.cskaoyan14th.controller;
 
-
-import com.cskaoyan14th.bean.COrder;
-import com.cskaoyan14th.bean.COrderExample;
 import com.cskaoyan14th.bean.UnqualifyApply;
 import com.cskaoyan14th.bean.UnqualifyApplyExample;
+import com.cskaoyan14th.mapper.FinalCountCheckMapper;
+import com.cskaoyan14th.mapper.ProcessCountCheckMapper;
 import com.cskaoyan14th.mapper.UnqualifyApplyMapper;
 import com.cskaoyan14th.util.MyUtil;
 import com.cskaoyan14th.vo.Vo;
@@ -15,33 +14,35 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 /**
  * @author Yuechao Yang
- * @version 2019-06-26-17:10
+ * @version 2019-06-27-14:56
  */
 @Controller
-@RequestMapping("unqualify")
-public class UnqualifyController {
+public class QualifyController {
     @Autowired
     UnqualifyApplyMapper unqualifyApplyMapper;
+    @Autowired
+    FinalCountCheckMapper finalCountCheckMapper;
+    @Autowired
+    ProcessCountCheckMapper processCountCheckMapper;
 
-    @RequestMapping("find")
-    public String find(HttpServletRequest request){
+    @RequestMapping("unqualify/find")
+    public String unqualifyFind(HttpServletRequest request){
         MyUtil.sysPermissionList(request);
         return "WEB-INF/jsp/unqualify_list";                                                                        /*这里面需要调用query写json返回*/
     }
-    @RequestMapping("list")                                                                                         /*用于数据回显，返回一个json的数据*/
+    @RequestMapping("unqualify/list")                                                                                         /*用于数据回显，返回一个json的数据*/
     @ResponseBody
-    public Vo<UnqualifyApply> list(int page, int rows){
+    public Vo<UnqualifyApply> unqualifyList(int page, int rows){
         PageHelper.startPage(page,rows);
         UnqualifyApplyExample unqualifyApplyExample = new UnqualifyApplyExample();
         UnqualifyApplyExample.Criteria criteria = unqualifyApplyExample.createCriteria();
         criteria.andProductIdIsNotNull();                                                                           /*这里是加了一个条件，用于不让productId为空*/
-            List<UnqualifyApply> unqualifyApplyList1 = unqualifyApplyMapper.selectByExample(unqualifyApplyExample); /*通过生成的example调用底层的mapper方法*/
+        List<UnqualifyApply> unqualifyApplyList1 = unqualifyApplyMapper.selectByExample(unqualifyApplyExample);     /*通过生成的example调用底层的mapper方法*/
 
         PageInfo<UnqualifyApply> pageInfo = new PageInfo<>(unqualifyApplyList1);
 
@@ -51,4 +52,32 @@ public class UnqualifyController {
         return unqualifyApplyList;
     }
 
+
+    @RequestMapping("f_count_check/find")
+    public String fCountCheckFind(HttpServletRequest request){
+        MyUtil.sysPermissionList(request);
+        return "WEB-INF/jsp/f_count_check_list";
+    }
+
+
+
+    @RequestMapping("measure/find")
+    public String fMeasureCheckFind(HttpServletRequest request){
+        MyUtil.sysPermissionList(request);
+        return "WEB-INF/jsp/measurement_list";
+    }
+
+
+    @RequestMapping("p_count_check/find")
+    public String pCountCheckFind(HttpServletRequest request){
+
+        MyUtil.sysPermissionList(request);
+        return "WEB-INF/jsp/p_count_check_list";
+    }
+
+    @RequestMapping("p_measure_check/find")
+    public String pMeasureCheckFind(HttpServletRequest request){
+        MyUtil.sysPermissionList(request);
+        return "WEB-INF/jsp/p_measure_check_list";
+    }
 }
