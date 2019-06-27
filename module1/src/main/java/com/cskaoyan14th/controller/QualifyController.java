dc84e51b3bf58ba0d10de5cb07478f4fd5c3e98c
baxigui <yyc05060808@gmail.com>
2019-06-27 15:48:26 +0800

    完成一三五小模块的json数据回显

--- module1/src/main/java/com/cskaoyan14th/controller/QualifyController.java
+++ module1/src/main/java/com/cskaoyan14th/controller/QualifyController.java
@@ -1,7 +1,6 @@
 package com.cskaoyan14th.controller;
 
-import com.cskaoyan14th.bean.UnqualifyApply;
-import com.cskaoyan14th.bean.UnqualifyApplyExample;
+import com.cskaoyan14th.bean.*;
 import com.cskaoyan14th.mapper.FinalCountCheckMapper;
 import com.cskaoyan14th.mapper.ProcessCountCheckMapper;
 import com.cskaoyan14th.mapper.UnqualifyApplyMapper;
@@ -30,12 +29,14 @@ public class QualifyController {
     @Autowired
     ProcessCountCheckMapper processCountCheckMapper;
 
-    @RequestMapping("unqualify/find")
+    /*不合格品管理*/
+    @RequestMapping("unqualify/find")                                                                               /*显示增删改按钮*/
     public String unqualifyFind(HttpServletRequest request){
         MyUtil.sysPermissionList(request);
         return "WEB-INF/jsp/unqualify_list";                                                                        /*这里面需要调用query写json返回*/
     }
-    @RequestMapping("unqualify/list")                                                                                         /*用于数据回显，返回一个json的数据*/
+
+    @RequestMapping("unqualify/list")                                                                               /*用于数据回显，返回一个json的数据*/
     @ResponseBody
     public Vo<UnqualifyApply> unqualifyList(int page, int rows){
         PageHelper.startPage(page,rows);
@@ -52,32 +53,71 @@ public class QualifyController {
         return unqualifyApplyList;
     }
 
-
-    @RequestMapping("f_count_check/find")
-    public String fCountCheckFind(HttpServletRequest request){
-        MyUtil.sysPermissionList(request);
-        return "WEB-INF/jsp/f_count_check_list";
-    }
-
-
-
-    @RequestMapping("measure/find")
+    /*成品计量质检*/
+    @RequestMapping("measure/find")                                                                                 /*显示增删改按钮*/
     public String fMeasureCheckFind(HttpServletRequest request){
         MyUtil.sysPermissionList(request);
         return "WEB-INF/jsp/measurement_list";
     }
 
 
-    @RequestMapping("p_count_check/find")
-    public String pCountCheckFind(HttpServletRequest request){
 
+    /*成品计数质检*/
+    @RequestMapping("f_count_check/find")                                                                           /*显示增删改按钮*/
+    public String fCountCheckFind(HttpServletRequest request){
         MyUtil.sysPermissionList(request);
-        return "WEB-INF/jsp/p_count_check_list";
+        return "WEB-INF/jsp/f_count_check_list";
     }
 
-    @RequestMapping("p_measure_check/find")
+
+    @RequestMapping("f_count_check/list")                                                                           /*用于数据回显，返回一个json的数据*/
+    @ResponseBody
+    public Vo<FinalCountCheck> finalCountCheckList(int page, int rows){
+        PageHelper.startPage(page,rows);
+        FinalCountCheckExample finalCountCheckExample = new FinalCountCheckExample();
+        FinalCountCheckExample.Criteria criteria = finalCountCheckExample.createCriteria();
+        criteria.andOrderIdIsNotNull();                                                                             /*这里是加了一个条件，用于不让productId为空*/
+        List<FinalCountCheck> finalCountCheckList1 = finalCountCheckMapper.selectByExample(finalCountCheckExample); /*通过生成的example调用底层的mapper方法*/
+
+        PageInfo<FinalCountCheck> pageInfo = new PageInfo<>(finalCountCheckList1);
+
+
+        Vo<FinalCountCheck> finalCountCheckList = new Vo<>(pageInfo.getTotal(),pageInfo.getList());
+        System.out.println(finalCountCheckList);
+        return finalCountCheckList;
+    }
+
+
+    /*工序计量质检*/
+    @RequestMapping("p_measure_check/find")                                                                         /*显示增删改按钮*/
     public String pMeasureCheckFind(HttpServletRequest request){
         MyUtil.sysPermissionList(request);
         return "WEB-INF/jsp/p_measure_check_list";
     }
+
+    /*工序计数质检*/
+    @RequestMapping("p_count_check/find")                                                                           /*显示增删改按钮*/
+    public String pCountCheckFind(HttpServletRequest request){
+        MyUtil.sysPermissionList(request);
+        return "WEB-INF/jsp/p_count_check_list";
+    }
+
+    @RequestMapping("p_count_check/list")                                                                           /*用于数据回显，返回一个json的数据*/
+    @ResponseBody
+    public Vo<ProcessCountCheck> ProcessCountCheckList(int page, int rows){
+        PageHelper.startPage(page,rows);
+        ProcessCountCheckExample processCountCheckExample = new ProcessCountCheckExample();
+        ProcessCountCheckExample.Criteria criteria = processCountCheckExample.createCriteria();
+        criteria.andProcessIdIsNotNull();                                                                           /*这里是加了一个条件，用于不让productId为空*/
+        List<ProcessCountCheck> processCountCheckList1 = processCountCheckMapper.selectByExample(processCountCheckExample);/*通过生成的example调用底层的mapper方法*/
+
+        PageInfo<ProcessCountCheck> pageInfo = new PageInfo<>(processCountCheckList1);
+
+
+        Vo<ProcessCountCheck> processCountCheckList = new Vo<>(pageInfo.getTotal(),pageInfo.getList());
+        System.out.println(processCountCheckList);
+        return processCountCheckList;
+    }
+
+
 }
