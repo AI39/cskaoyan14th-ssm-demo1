3b944c7f4b022bd942a2025471873eada462671e
baxigui <yyc05060808@gmail.com>
2019-06-27 10:07:13 +0800

    删除utils文件夹的工具类，以及把impl实现类放在指定文件夹

---
+++ module1/src/main/java/com/cskaoyan14th/service/impl/UnqualifyServiceImpl.java
@@ -0,0 +1,11 @@
+package com.cskaoyan14th.service.impl;
+
+import com.cskaoyan14th.service.UnqualifyService;
+
+/**
+ * @author Yuechao Yang
+ * @version 2019-06-26-17:12
+ */
+public class UnqualifyServiceImpl implements UnqualifyService {
+
+}
