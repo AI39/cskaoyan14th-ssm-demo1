a3090b6e3e7d2aa9fa2c2fd980ae505118e9dbdc
huangshuang221b <1700890377@qq.com>
2019-06-28 11:12:54 +0800

    hs

--- module1/src/main/java/com/cskaoyan14th/service/MaterialService.java
+++ module1/src/main/java/com/cskaoyan14th/service/MaterialService.java
@@ -1,6 +1,7 @@
 package com.cskaoyan14th.service;
 
 import com.cskaoyan14th.bean.Material;
+import com.cskaoyan14th.bean.MaterialReceive;
 import com.cskaoyan14th.vo.Vo;
 
 public interface MaterialService {
@@ -19,6 +20,8 @@ public interface MaterialService {
     Vo<Material> selectMaterialVoById(String id,int page,int rows);
 
     Vo<Material> selectMaterialVoByType(String type,int page,int rows);
+
+    Vo<MaterialReceive> getMaterialReceiveVo(int page, int rows);
 
 
 }
